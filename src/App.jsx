1350b0d0507842b93fab05c5930508af2f70ae28
Benjamin Zhao <benzql@gmail.com>
2024-09-06 22:46:56 +1000

    add 'ShowProject' and modify Creataproject and App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Createprojects from "./components/Createprojects";
 import Noprojects from "./components/Noprojects";
+import ShowProject from "./components/ShowProject";
+
 import { useState } from "react";
 
 function App() {
-  let projects = [
-    {projectName: "Learning Reactive"},
-  ]
+  const [projects, setProjects] = useState([]);
+
   const [hasNoProjects, setHasNoProjects] = useState(true)
   const [hasStartedNewProject, setHasStartedNewProject] = useState(false)
 
@@ -14,10 +15,17 @@ function App() {
     setHasStartedNewProject(true);
   }
 
-  function saveNewProject(){
-    alert("OK");
+  function saveNewProject(newProject){
+    setProjects([...projects, newProject]);
+    console.log(newProject);
   }
 
+  const sampleProject = {
+    title: "Learning Project",
+    dueDate: "Dec 29, 2024",
+    description: "Learn React from the group up. \n\nStart with the basics, finish with advanced knowledge.",
+    tasks:['Practice, practice, practice', 'Learn advanced concepts', 'Learn the basics'],
+  }
   
   return (
     <>
@@ -25,16 +33,17 @@ function App() {
           <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
             <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
             <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100" onClick={createProject}>+ Add Project</button>
-            {projects.length >0 && 
+            {projects.length > 0 && 
               <ul className="mt-8">
                 {projects.map((p)=>
-                  <li className="px-4 py-2 hover:bg-stone-800 text-stone-400 hover:text-stone-100">{p.projectName}</li>
+                  <li className="px-4 py-2 hover:bg-stone-800 text-stone-400 hover:text-stone-100">{p.title}</li>
                 )}
               </ul>
             }
           </aside>
           {hasNoProjects && <Noprojects handleClick={createProject}/>}
           {hasStartedNewProject && <Createprojects handelSaveNewProject={saveNewProject}/>}
+          <ShowProject project={sampleProject}/>
         </main>
     </>
   );
--- src/components/Createprojects.jsx
+++ src/components/Createprojects.jsx
@@ -1,17 +1,36 @@
+import { useRef, useState } from "react";
+
 export default function Createprojects({handelSaveNewProject}){
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+    
+
     return(
         <div className="w-3/4 mt-16 mr-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-                <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={()=>handelSaveNewProject()}>Save</button>
+                <button 
+                    className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" 
+                    onClick={()=>handelSaveNewProject(
+                        {
+                            title: title.current.value,
+                            description: description.current.value,
+                            dueDate: dueDate.current.value,
+                            tasks:[]
+                        }
+                    )}>
+                    Save
+                </button>
             </menu>
             <section>
                 <label className="text-sm font-bold uppercase text-stone-500">TITLE</label>
-                <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
+                <input ref={title} className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
                 <label className="text-sm font-bold uppercase text-stone-500">DESCRIPTION</label>
-                <textarea className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"></textarea>
+                <textarea rows='10' ref={description} className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"></textarea>
                 <label className="text-sm font-bold uppercase text-stone-500">DUE DATE</label>
-                <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" type="date"/>
+                <input ref={dueDate} className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" type="date"/>
             </section>
         </div>
     );
